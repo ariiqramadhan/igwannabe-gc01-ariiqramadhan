652973f33f652d2aacf6b4eabc5572ff4e51beb5
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-20 15:05:37 +0700

    fix: add validation in follow user

--- resolvers/follow.js
+++ resolvers/follow.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require("graphql");
 const { ObjectId } = require("mongodb");
 
 const resolvers = {
@@ -8,11 +9,27 @@ const resolvers = {
 
             const { id } = await authentication();
 
+            if (newFollow.followingId === id) {
+                throw new GraphQLError('You cannot follow yourself', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    }
+                });
+            }
+
             newFollow.followingId = new ObjectId(newFollow.followingId);
             newFollow.followerId = new ObjectId(id);
             newFollow.createdAt = newFollow.updatedAt = new Date();
 
             const follows = db.collection('Follows');
+            const isFollow = await follows.findOne({ followingId: newFollow.followingId, followerId: newFollow.followerId });
+            if (isFollow) {
+                throw new GraphQLError('You already follow this user', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    }
+                });
+            }
             await follows.insertOne(newFollow);
 
             return newFollow;
