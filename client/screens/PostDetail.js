eab7f579091a6fda17e9e9d427974b0ed68495a5
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-24 19:38:48 +0700

    fix: change post detail design

--- client/screens/PostDetail.js
+++ client/screens/PostDetail.js
@@ -4,6 +4,9 @@ import Feather from '@expo/vector-icons/Feather';
 import {
     Image,
     Keyboard,
+    KeyboardAvoidingView,
+    Modal,
+    Platform,
     Pressable,
     ScrollView,
     StyleSheet,
@@ -14,20 +17,23 @@ import {
     View,
 } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useMutation, useQuery } from '@apollo/client';
 import { COMMENT_POST, GET_POST, LIKE_POST } from '../queries/query';
 import { useRoute } from '@react-navigation/native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const black = '#080814';
 
 export default function PostDetail() {
+    const [modalVisible, setModalVisible] = useState(false);
     const [userComment, setUserComment] = useState('');
     const route = useRoute();
     const { id } = route.params;
     const [like, likeObj] = useMutation(LIKE_POST, {
         refetchQueries: [GET_POST],
     });
-
     const [comment, commentObj] = useMutation(COMMENT_POST, {
         refetchQueries: [GET_POST],
     });
@@ -68,7 +74,7 @@ export default function PostDetail() {
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <ScrollView style={{flex: 1, backgroundColor: '#FFF'}}>
+            <ScrollView style={{ flex: 1, backgroundColor: '#FFF' }}>
                 <View
                     style={{
                         flex: 1,
@@ -83,9 +89,7 @@ export default function PostDetail() {
                                 source={require('../assets/blank-pp.jpg')}
                                 style={{ width: 44, height: 44 }}
                             />
-                            <Text
-                                style={{ fontWeight: '600', color: '#080814' }}
-                            >
+                            <Text style={{ fontWeight: '600', color: black }}>
                                 {data?.GetPost.author.username}
                             </Text>
                         </View>
@@ -98,7 +102,7 @@ export default function PostDetail() {
                             <Entypo
                                 name="dots-three-horizontal"
                                 size={20}
-                                color="#080814"
+                                color={black}
                             />
                         </View>
                     </View>
@@ -118,12 +122,10 @@ export default function PostDetail() {
                                     <AntDesign
                                         name="hearto"
                                         size={26}
-                                        color="#080814"
+                                        color={black}
                                     />
                                 </TouchableHighlight>
-                                <Text
-                                    style={{ fontSize: 16, color: '#080814' }}
-                                >
+                                <Text style={{ fontSize: 16, color: black }}>
                                     {data?.GetPost.likes.length}
                                 </Text>
                             </View>
@@ -131,23 +133,15 @@ export default function PostDetail() {
                                 <MaterialCommunityIcons
                                     name="comment-outline"
                                     size={26}
-                                    color="#080814"
+                                    color={black}
                                 />
-                                <Text
-                                    style={{ fontSize: 16, color: '#080814' }}
-                                >
+                                <Text style={{ fontSize: 16, color: black }}>
                                     {data?.GetPost.comments.length}
                                 </Text>
                             </View>
                             <View style={styles.footLogo}>
-                                <Feather
-                                    name="send"
-                                    size={26}
-                                    color="#080814"
-                                />
-                                <Text
-                                    style={{ fontSize: 16, color: '#080814' }}
-                                >
+                                <Feather name="send" size={26} color={black} />
+                                <Text style={{ fontSize: 16, color: black }}>
                                     0
                                 </Text>
                             </View>
@@ -155,7 +149,7 @@ export default function PostDetail() {
                         <FontAwesome
                             name="bookmark-o"
                             size={24}
-                            color="#080814"
+                            color={black}
                         />
                     </View>
                     <View style={{ width: '100%', paddingHorizontal: 8 }}>
@@ -172,35 +166,178 @@ export default function PostDetail() {
                             {data?.GetPost.tags?.map((tag) => `#${tag} `)}
                         </Text>
                     </View>
-                    <View style={{ paddingHorizontal: 8, marginTop: 20 }}>
-                        {data?.GetPost.comments.map((val, i) => {
-                            return (
-                                <View key={i} style={{ marginBottom: 8 }}>
+                    <View style={{ paddingHorizontal: 8 }}>
+                        <Text style={{ fontWeight: '600' }}>
+                            {data?.GetPost.comments[0]?.username}{' '}
+                            <Text style={{ fontWeight: 'normal' }}>
+                                {data?.GetPost.comments[0]?.content}
+                            </Text>
+                        </Text>
+                        <Pressable
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={{ marginTop: 4, color: '#9D9D9D' }}>
+                                View all comments
+                            </Text>
+                        </Pressable>
+                    </View>
+                    <Modal
+                        animationType="slide"
+                        visible={modalVisible}
+                        transparent={true}
+                    >
+                        <View style={styles.modal}>
+                            <TouchableWithoutFeedback
+                                onPress={() => setModalVisible(!modalVisible)}
+                            >
+                                <View
+                                    style={{
+                                        height: '40%',
+                                        width: '100%',
+                                    }}
+                                ></View>
+                            </TouchableWithoutFeedback>
+                            <View style={styles.commentSection}>
+                                <View style={styles.commentSectionHeader}>
                                     <Text
                                         style={{
+                                            fontSize: 18,
                                             fontWeight: '600',
-                                            color: '#080814',
                                         }}
                                     >
-                                        {val.username}
-                                    </Text>
-                                    <Text style={{ color: '#080814' }}>
-                                        {val.content}
+                                        Comments
                                     </Text>
                                 </View>
-                            );
-                        })}
-                    </View>
-                    <View style={{ paddingHorizontal: 8 }}>
-                        <TextInput
-                            placeholder="Add a comment..."
-                            onChangeText={setUserComment}
-                            onSubmitEditing={handleComment}
-                            autoCapitalize={false}
-                            autoCorrect={false}
-                            value={userComment}
-                        />
-                    </View>
+                                <View
+                                    style={{
+                                        flex: 8,
+                                        paddingHorizontal: 8,
+                                        paddingTop: 20,
+                                        paddingBottom: 80,
+                                    }}
+                                >
+                                    <ScrollView>
+                                        <View style={{flex: 1}}>
+                                            {data?.GetPost.comments.map(
+                                                (val, i) => {
+                                                    return (
+                                                        <View
+                                                            key={i}
+                                                            style={{
+                                                                marginBottom: 8,
+                                                            }}
+                                                        >
+                                                            <View
+                                                                style={
+                                                                    styles.usernameComment
+                                                                }
+                                                            >
+                                                                <View>
+                                                                    <Image
+                                                                        source={require('../assets/blank-pp.jpg')}
+                                                                        style={{
+                                                                            width: 44,
+                                                                            height: 44,
+                                                                        }}
+                                                                    />
+                                                                </View>
+                                                                <View>
+                                                                    <Text
+                                                                        style={{
+                                                                            fontWeight:
+                                                                                '600',
+                                                                            color: black,
+                                                                        }}
+                                                                    >
+                                                                        {
+                                                                            data
+                                                                                ?.GetPost
+                                                                                .author
+                                                                                .username
+                                                                        }
+                                                                    </Text>
+                                                                    <Text
+                                                                        style={{
+                                                                            color: '#080814',
+                                                                        }}
+                                                                    >
+                                                                        {
+                                                                            val.content
+                                                                        }
+                                                                    </Text>
+                                                                </View>
+                                                            </View>
+                                                        </View>
+                                                    );
+                                                }
+                                            )}
+                                        </View>
+                                    </ScrollView>
+                                </View>
+                            </View>
+                            <KeyboardAvoidingView
+                                behavior={
+                                    Platform.OS === 'ios' ? 'padding' : 'height'
+                                }
+                                style={{
+                                    position: 'absolute',
+                                    bottom: 0,
+                                    right: 0,
+                                    left: 0,
+                                    backgroundColor: '#FFF',
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        width: '100%',
+                                        paddingHorizontal: 8,
+                                        backgroundColor: '#FFF',
+                                        marginBottom: 40,
+                                    }}
+                                >
+                                    <View style={styles.addComment}>
+                                        <Image
+                                            source={require('../assets/blank-pp.jpg')}
+                                            style={{ width: 44, height: 44 }}
+                                        />
+                                        <View
+                                            style={{
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                gap: 8,
+                                                justifyContent: 'space-between',
+                                                flex: 1,
+                                            }}
+                                        >
+                                            <View style={{ flex: 1 }}>
+                                                <TextInput
+                                                    placeholder="Add Comments..."
+                                                    placeholderTextColor="#9D9D9D"
+                                                    style={
+                                                        styles.addCommentForm
+                                                    }
+                                                    onChangeText={
+                                                        setUserComment
+                                                    }
+                                                    onSubmitEditing={
+                                                        handleComment
+                                                    }
+                                                    autoCapitalize={false}
+                                                    autoCorrect={false}
+                                                    value={userComment}
+                                                />
+                                            </View>
+                                            <Ionicons
+                                                name="send"
+                                                size={24}
+                                                color={black}
+                                            />
+                                        </View>
+                                    </View>
+                                </View>
+                            </KeyboardAvoidingView>
+                        </View>
+                    </Modal>
                 </View>
             </ScrollView>
         </TouchableWithoutFeedback>
@@ -235,5 +372,47 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         gap: 5,
+    },
+    modal: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    commentSection: {
+        height: '60%',
+        width: '100%',
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    commentSectionHeader: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 0.5,
+        width: '100%',
+        borderColor: '#9D9D9D',
+    },
+    usernameComment: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        gap: 8,
+    },
+    addComment: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+        width: '100%',
+    },
+    addCommentForm: {
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderRadius: 30,
+        borderColor: '#A6A6A6',
+        padding: 10,
+        color: black,
     },
 });
